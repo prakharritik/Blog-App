307d85a16e7609283cd441ea2407b5e5fde03805
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-03-21 17:56:28 +0530

    Initial Setup.

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
