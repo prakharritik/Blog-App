14c7c49bb2a9ad2fcd9d68cb04861dcbbd1f9bbb
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-03-22 19:12:48 +0530

    Setup context to move data in application.

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
+import { StyleSheet, Text, View, FlatList, Button } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
+
   return (
     <View>
-      <Text>Index Screen</Text>
+      <Text>Index Screen </Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
